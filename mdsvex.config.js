6b77eb3d617362289f7dc6c2ddb90a48573689f6
二叉树树 <acofork@qq.com>
2026-04-21 20:36:19 +0800

    chore: 移除 mdsvex 和 rehype-pretty-code 的调试日志

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,30 +1,23 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 import rehypePrettyCode from 'rehype-pretty-code';
 
-console.log('[mdsvex] 开始加载配置...');
-
 const prettyCodeOptions = {
 	theme: {
 		light: 'github-light',
 		dark: 'github-dark'
 	},
 	onVisitLine(node) {
-		console.log('[rehype-pretty-code] 访问代码行:', node);
 		if (node.children.length === 0) {
 			node.children = [{ type: 'text', value: ' ' }];
 		}
 	},
 	onVisitHighlightedLine(node) {
-		console.log('[rehype-pretty-code] 访问高亮行:', node);
 		node.properties.className = ['line--highlighted'];
 	},
 	onVisitHighlightedWord(node) {
-		console.log('[rehype-pretty-code] 访问高亮词:', node);
 		node.properties.className = ['word--highlighted'];
 	}
 };
-
-console.log('[mdsvex] rehype-pretty-code 配置:', prettyCodeOptions);
 
 const config = defineConfig({
 	extensions: ['.md'],
@@ -33,9 +26,7 @@ const config = defineConfig({
 	},
 	remarkPlugins: [],
 	rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
-	highlight: false // 禁用默认的 Prism.js 高亮
+	highlight: false
 });
-
-console.log('[mdsvex] 配置加载完成');
 
 export default config;

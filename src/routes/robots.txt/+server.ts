a90dd7bb588b44b11e3c7c57ff8526eb51c3ea1e
二叉树树 <acofork@qq.com>
2026-04-22 10:58:50 +0800

    feat: 添加sitemap.xml和robots.txt自动生成

---
+++ src/routes/robots.txt/+server.ts
@@ -0,0 +1,19 @@
+import { siteConfig } from '$lib/config/site';
+import type { RequestHandler } from './$types';
+
+export const prerender = true;
+
+export const GET: RequestHandler = async () => {
+	const robots = `User-agent: *
+Allow: /
+
+Sitemap: ${siteConfig.url}/sitemap.xml
+`;
+
+	return new Response(robots, {
+		headers: {
+			'Content-Type': 'text/plain',
+			'Cache-Control': 'max-age=0, s-maxage=3600'
+		}
+	});
+};

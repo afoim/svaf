839f7eb64d29fdabd08e42bffe85301fe336905e
二叉树树 <acofork@qq.com>
2026-04-22 12:17:28 +0800

    feat: add NAT type detection page using WebRTC + STUN

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -207,6 +207,13 @@
 			</Button>
 		</a>
 		
+		<a href="/nat">
+			<Button variant="outline" class="flex items-center gap-2">
+				<Icon icon="mdi:lan-check" class="w-5 h-5" />
+				NAT 检测
+			</Button>
+		</a>
+		
 		<a href="/friends">
 			<Button variant="outline" class="flex items-center gap-2">
 				<Icon icon="mdi:link-variant" class="w-5 h-5" />

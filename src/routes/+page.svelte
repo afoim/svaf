92d9f9a9ef346e6c129951e88b9e945b9fd09d1a
二叉树树 <acofork@qq.com>
2026-04-22 12:13:44 +0800

    feat: add long link generator using i/I binary encoding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -200,6 +200,13 @@
 			</Button>
 		</a>
 		
+		<a href="/long">
+			<Button variant="outline" class="flex items-center gap-2">
+				<Icon icon="mdi:link-variant-plus" class="w-5 h-5" />
+				长链
+			</Button>
+		</a>
+		
 		<a href="/friends">
 			<Button variant="outline" class="flex items-center gap-2">
 				<Icon icon="mdi:link-variant" class="w-5 h-5" />
